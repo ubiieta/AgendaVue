f77a97dc548572625cd75617ea7bc22577bdfd14
ubiieta <aitorubieta2004@gmail.com>
2024-11-29 10:57:44 +0100

    cdn

--- firebase.js
+++ firebase.js
@@ -1,8 +1,27 @@
 
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCHTCjVWRmVx_AW6CeKLxoeQGfHvhFMoaI",
+//   authDomain: "agendavue-7bb62.firebaseapp.com",
+//   projectId: "agendavue-7bb62",
+//   storageBucket: "agendavue-7bb62.firebasestorage.app",
+//   messagingSenderId: "40314208916",
+//   appId: "1:40314208916:web:c32fb6f2acf6b1bfb02a81"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Importa Firestore
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,3 +35,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Export Firestore instance
+export { db };
